c965926f7c7c409b798efd848205895473f7a7b2
Анисько Дмитрий Геннадьевич <vorobushek221@gmail.com>
2017-08-20 01:22:25 +0300

    Entity and ViewModel added.

---
+++ BpmContactManager/Models/ViewModels/ContactViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BpmContactManager.Models.ViewModels
+{
+    public class ContactViewModel
+    {
+        public int Id { get; set; }
+
+        public string MobilePhone { get; set; }
+
+        public string Dear { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
