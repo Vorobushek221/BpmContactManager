bc1a715934457a203c578900057d69c72674dd06
Анисько Дмитрий Геннадьевич <vorobushek221@gmail.com>
2017-08-20 19:21:24 +0300

    GlobalConstants class added.

---
+++ BpmContactManager/Models/GlobalConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BpmContactManager.Models
+{
+    public static class GlobalConstants
+    {
+        public const string ServerUri = @"http://dev.altaras.ru:8001/0/ServiceModel/EntityDataService.svc";
+
+        public const string AuthServiceUri = @"http://dev.altaras.ru:8001/serviceModel/AuthService.svc/Login";
+
+        public const string ServiceLogin = @"Supervisor";
+
+        public const string ServicePassord = @"Supervisor__";
+    }
+}
