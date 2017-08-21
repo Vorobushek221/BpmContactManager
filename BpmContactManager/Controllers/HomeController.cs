ef489227da3dbd3c3bdb50f4aaddc35ea8d52e17
Анисько Дмитрий Геннадьевич <vorobushek221@gmail.com>
2017-08-21 20:24:01 +0300

    Adedd Index view. Also created contact ajax uploading.

---
+++ BpmContactManager/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using BpmContactManager.Models;
+using BpmContactManager.Models.Entities;
+using BpmContactManager.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BpmContactManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Contacts(int count, int offset)
+        {
+            var contactServiceManager = new ContactServiceManager();
+            var contactEntityList = contactServiceManager.GetContacts(count, offset);
+            var contactViewModelList = new List<ContactViewModel>();
+
+            foreach (var contactEntity in contactEntityList)
+            {
+                contactViewModelList.Add(contactEntity.ToViewModel());
+            }
+            return Json(contactViewModelList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ BpmContactManager/Scripts/rowUploaded.js
@@ -0,0 +1,59 @@
+﻿var dynamicLoadContactCount = 15;
+var defaultOffset = 0;
+var defaultUrl = "/Home/Contacts";
+
+function getContacts(count, offset, url) {
+    url = url || defaultUrl;
+    count = count || dynamicLoadContactCount;
+    offset = offset || defaultOffset;
+    $.getJSON(url, { count: count, offset: offset }, addRows);
+}
+
+function addRows(contacts) {
+    contacts.forEach(function (contact) {
+        addTableRow(contact);
+    });
+}
+
+function addTableRow(contact) {
+    var row = $('<tr>', {
+        class: 'info-row'
+    })
+    .append($('<td>', {
+        class: 'name',
+        text: contact.Name
+    }))
+    .append($('<td>', {
+        class: 'dear',
+        text: contact.Dear
+    }))
+    .append($('<td>', {
+        class: 'mobile-phone',
+        text: contact.MobilePhone
+    }))
+    .append($('<td>', {
+        class: 'job-title',
+        text: contact.JobTitle
+    }))
+    .append($('<td>', {
+        class: 'birth-date',
+        text: contact.BirthDate
+    }));
+
+    var actionsSection = $('<td>', {
+        class: 'actions',
+    })
+    .append($('<a>', {
+        text: 'Edit',
+        href: '/Home/Edit/' + contact.ServiceId
+    }))
+    .append(' | ')
+    .append($('<a>', {
+        text: 'Delete',
+        href: '/Home/Delete/' + contact.ServiceId
+    }));
+
+    row.append(actionsSection);
+
+    $('tbody').append(row);
+}
